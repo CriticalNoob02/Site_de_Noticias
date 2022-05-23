8d098c03b6962b1fdd099b0fa871013554bebdec
AdrielDevelop <adrieldchavea20@gmail.com>
2022-05-23 10:41:21 -0300

    B-D_com_objetoJS

--- app/public/JS/script.js
+++ app/public/JS/script.js
@@ -1,1 +1,10 @@
 //Comentário apenas para saber se consegui linkar
+
+const url_atual = window.location.pathname
+
+if(url_atual == "/"){
+    document.getElementById ("home-menu").className = "nav-link text-white active"
+}
+else if(url_atual == "/noticias"){
+    document.getElementById ("noticias-menu").className = "nav-link text-white active"
+}
