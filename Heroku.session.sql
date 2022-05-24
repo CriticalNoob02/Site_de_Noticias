350248e3787b57e03b446532a8c5af64c692addc
AdrielDevelop <adrieldchavea20@gmail.com>
2022-05-24 18:50:23 -0300

    Limpando_db

--- Heroku.session.sql
+++ Heroku.session.sql
@@ -1,3 +1,5 @@
 --create table noticias(id_noticia serial primary key, titulo varchar(100) not null, conteudo text not null, data_criacao timestamp without time zone default (now() at time zone 'utc+3'))
 --insert into noticias(titulo, conteudo) VALUES ('Banco de dados online', 'Foi realizado a primeira inserção de dados na tabela')
-select * from noticias
+SELECT * from noticias
+
+
